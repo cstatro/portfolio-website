f430665677c2102f97e66d3f59a97590525dfc64
Colin <cstatro@gmail.com>
2019-11-25 20:19:59 -0500

    icons are now working nice

--- src/RightPanel.jsx
+++ src/RightPanel.jsx
@@ -5,6 +5,17 @@ class RightPanel extends Component {
   render() {
     return (
       <div className="right-panel">
+        <div className="corner-icons">
+          <a
+            className="contact-icon linked-in"
+            href="https://www.linkedin.com/in/colin-tatro/"
+          ></a>
+          <a
+            className="contact-icon git-hub"
+            href="https://github.com/cstatro"
+          ></a>
+          <a className="contact-icon email" href="mailto:cstatro@gmail.com"></a>
+        </div>
         <Fade>
           <div className="greeting">Hello There</div>
         </Fade>
