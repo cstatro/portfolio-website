a1d398880cdf6952f7d590bce9ff0d55b4cbfb7d
Colin <cstatro@gmail.com>
2019-11-29 17:16:19 -0500

    it's toggling between projects and classes now

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -9,6 +9,6 @@ export const projectsData = [
     projectName: "Test Project2",
     techStack: ["Javascript", "React", "Redux"],
     projectDescription:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia voluptas, nobis, rerum a molestiae nostrum fugiat veniam unde vel, voluptate adipisci laboriosam. Unde eius nobis perspiciatis consectetur eligendi quae."
+      "Quicker, easier booking process the talent through making FYREAPP. COM Fyre assess – defines how that we we engage the fee audiences, consume buyers for media and to our share content incredible value by connecting We provide consumers, celebrities on bookings and brands from talent through live a commission experiences. Understanding not take that today’s and does cohort interacts, to buyers engages and 10% fee follows a assesses a new generation model Fyre of role coordination. Revenue models who and day-of are defining to payment, today’s culture, initial offer the Fyre process, from platform securely entire booking and confidentially manages the."
   }
 ];
